7c756224c4d312d9faff7fdda173a4561be5d39b
podarok <podarok@ua.fm>
2017-03-03 19:58:36 +0200

    Pusher is ready

--- docroot/modules/custom/dranks_sync/src/DrupalProxy.php
+++ docroot/modules/custom/dranks_sync/src/DrupalProxy.php
@@ -9,10 +9,89 @@
 namespace Drupal\dranks_sync;
 
 
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Entity\Query\QueryFactory;
+use Drupal\Core\Logger\LoggerChannelInterface;
+
 class DrupalProxy implements DrupalProxyInterface{
 
+
+  /**
+   * Max allowed updates per run.
+   */
+  const PROXY_UPDATE_PER_RUN = 100;
+
+  /**
+   * Max children for single parent entity.
+   */
+  const MAX_CHILD_WARNING = 100;
+
+  /**
+   * Entity load chunk.
+   */
+  const ENTITY_LOAD_CHUNK = 100;
+
+  /**
+   * @var \Drupal\dranks_sync\Wrapper
+   */
+  protected $dataWrapper;
+
+  /**
+   * Timezone object.
+   *
+   * @var \DateTimeZone
+   */
+  protected $timezone;
+
+  /**
+   * Query factory.
+   *
+   * @var QueryFactory
+   */
+  protected $queryFactory;
+
+  /**
+   * Logger.
+   *
+   * @var LoggerChannelInterface
+   */
+  protected $logger;
+
+  /**
+   * @var \Drupal\dranks_sync\Fetcher
+   */
+  protected $fetcher;
+
+  /**
+   * Entity type manager.
+   *
+   * @var EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+  /**
+   * The cache storage.
+   *
+   * @var \Drupal\Core\Entity\EntityStorageInterface
+   */
+  protected $cacheStorage;
+
+  public function __construct(Wrapper $data_wrapper, QueryFactory $query_factory, LoggerChannelInterface $logger, Fetcher $fetcher, EntityTypeManagerInterface $entity_type_manager) {
+    $this->dataWrapper = $data_wrapper;
+    $this->queryFactory = $query_factory;
+    $this->logger = $logger;
+    $this->fetcher = $fetcher;
+    $this->entityTypeManager = $entity_type_manager;
+
+    $this->cacheStorage = $this->entityTypeManager->getStorage(Wrapper::ENTITY_TYPE);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
   public function saveEntities() {
     // TODO: Implement saveEntities() method.
+    $this->logger->error($this->t('Implement saveEntities method'));
   }
 
 }
--- docroot/modules/custom/dranks_sync/src/Pusher.php
+++ docroot/modules/custom/dranks_sync/src/Pusher.php
@@ -9,6 +9,96 @@
 namespace Drupal\dranks_sync;
 
 
-class Pusher extends PusherInterface{
+use Drupal\Core\Config\ConfigFactory;
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Entity\Query\QueryFactory;
+use Drupal\Core\Entity\Query\QueryFactoryInterface;
+use Drupal\Core\Logger\LoggerChannelInterface;
+
+class Pusher implements PusherInterface{
+
+
+  /**
+   * Wrapper to be used.
+   *
+   * @var Wrapper
+   */
+  protected $dataWrapper;
+
+  /**
+   * Config Factory.
+   *
+   * @var ConfigFactory
+   */
+  protected $configFactory;
+
+
+  /**
+   * The logger channel.
+   *
+   * @var LoggerChannelInterface
+   */
+  protected $logger;
+
+  /**
+   * Entity type manager.
+   *
+   * @var EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+  /**
+   * Proxy.
+   *
+   * @var DrupalProxy
+   */
+  protected $proxy;
+
+  /**
+   * Query factory.
+   *
+   * @var QueryFactory
+   */
+  protected $query;
+
+  /**
+   * The entity storage.
+   *
+   * @var \Drupal\Core\Entity\EntityStorageInterface
+   */
+  protected $cacheStorage;
+
+  /**
+   * Pusher constructor.
+   *
+   * @param Wrapper $data_wrapper
+   *   Data wrapper.
+   * @param ConfigFactoryInterface $config_factory
+   *   Config Factory.
+   * @param LoggerChannelInterface $logger
+   *   The logger channel.
+   * @param EntityTypeManagerInterface $entity_type_manager
+   *   Entity type manager.
+   * @param DrupalProxyInterface $proxy
+   *   The Proxy.
+   * @param QueryFactoryInterface $query
+   *   Query factory.
+   */
+  public function __construct(Wrapper $data_wrapper, ConfigFactoryInterface $config_factory, LoggerChannelInterface $logger, EntityTypeManagerInterface $entity_type_manager, DrupalProxyInterface $proxy, QueryFactoryInterface $query) {
+    $this->dataWrapper = $data_wrapper;
+    $this->configFactory = $config_factory;
+    $this->logger = $logger;
+    $this->entityTypeManager = $entity_type_manager;
+    $this->proxy = $proxy;
+    $this->query = $query;
+
+    $this->cacheStorage = $this->entityTypeManager->getStorage(Wrapper::ENTITY_TYPE);
+
+  }
+
+  public function push() {
+    // TODO: Implement push() method.
+  }
 
 }
